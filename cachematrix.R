f4f9e02d342dc1112227508be5b4dbac5ca64769
Devin Bailey <devin.bailey@biogen.com>
2021-11-11 11:07:41 -0500

    Updated Comments 1:6,28

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions work in tandem to store and
+## recall a calculated inverse matrix.  This storage reduces processing time
+## for routinely used matrix calculations
 
-## Write a short comment describing this function
-
+# Function stores a specified matrix and calculated inverse in Parent Environment
+# storage allows for easy recall and eliminate constant processing with solve()
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL #Initialize m Variable
+  
+  #In parent environment, store X and clear m from history
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #Get values from parent environment then calculate/store means
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  
+  #Create a named list to store function values
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# Specialized solve function to return stored inverse matrix from makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse() #Attempt to retrieve Stored Inverse
+  
+  #If Value is stored, return the Inverse Matrix from storage
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  
+  #If Not stored, retrieve Input and Calculate Inverse
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+
+message("Test Functions")
+m1<-matrix(c(0.5, -0.25, -1.0, .75), nrow =2, ncol=2)
+m1
+Test_Matrix1<-makeCacheMatrix(m1)
+
+
